47b897e0a6ee5799a1d198eea0fe5bbe4cbfff2e
HaroldDRF <95robots@gmail.com>
2022-03-25 21:12:04 -0700

    substitution tests completed

--- test/substitution.test.js
+++ test/substitution.test.js
@@ -3,80 +3,80 @@ const { substitution } = require("../src/substitution");
 
 describe("substitution tests", () => {
   describe("error handling", () => {
+    let message = "message";
+    let alphabet = "short";
+
     it("should return false if the substitution alphabet is missing", () => {
-      const message = "message";
       const actual = substitution(message);
       expect(actual).to.be.false;
     });
 
     it("should return false if the substitution alphabet is not exactly 26 characters", () => {
-      const message = "message";
-      const alphabet = "short";
       const actual = substitution(message, alphabet);
       expect(actual).to.be.false;
     });
 
     it("should return false if the substitution alphabet does not contain unique characters", () => {
-      const message = "message";
-      const alphabet = "abcabcabcabcabcabcabcabcab";
+      alphabet = "abcabcabcabcabcabcabcabcab";
       const actual = substitution(message, alphabet);
       expect(actual).to.be.false;
     });
   });
 
-  describe("encoding a message", () => {
+  describe("encode message", () => {
+    let message = "message";
+    let alphabet = "short";
+    let expected = "ykrrpik";
+
     it("should encode a message by using the given substitution alphabet", () => {
-      const message = "message";
-      const alphabet = "plmoknijbuhvygctfxrdzeswaq";
+      alphabet = "plmoknijbuhvygctfxrdzeswaq";
       const actual = substitution(message, alphabet);
-      const expected = "ykrrpik";
 
       expect(actual).to.equal(expected);
     });
 
     it("should work with any kind of key with unique characters", () => {
-      const message = "message";
-      const alphabet = ".waeszrdxtfcygvuhbijnokmpl";
+      alphabet = ".waeszrdxtfcygvuhbijnokmpl";
       const actual = substitution(message, alphabet);
-      const expected = "ysii.rs";
+      expected = "ysii.rs";
 
       expect(actual).to.equal(expected);
     });
 
     it("should preserve spaces", () => {
-      const message = "my message";
-      const alphabet = ".waeszrdxtfcygvuhbijnokmpl";
+      message = "my message";
+      alphabet = ".waeszrdxtfcygvuhbijnokmpl";
       const actual = substitution(message, alphabet);
-      const expected = "yp ysii.rs";
+      expected = "yp ysii.rs";
 
       expect(actual).to.equal(expected);
     });
   });
 
-  describe("decoding a message", () => {
+  describe("decode message", () => {
+    let message = "ykrrpik";
+    let alphabet = "plmoknijbuhvygctfxrdzeswaq";
+    let expected = "message";
     it("should decode a message by using the given substitution alphabet", () => {
-      const message = "ykrrpik";
-      const alphabet = "plmoknijbuhvygctfxrdzeswaq";
       const actual = substitution(message, alphabet, false);
-      const expected = "message";
 
       expect(actual).to.equal(expected);
     });
 
     it("should work with any kind of key with unique characters", () => {
-      const message = "ysii.rs";
-      const alphabet = ".waeszrdxtfcygvuhbijnokmpl";
+      message = "ysii.rs";
+      alphabet = ".waeszrdxtfcygvuhbijnokmpl";
       const actual = substitution(message, alphabet, false);
-      const expected = "message";
+      expected = "message";
 
       expect(actual).to.equal(expected);
     });
 
     it("should preserve spaces", () => {
-      const message = "yp ysii.rs";
-      const alphabet = ".waeszrdxtfcygvuhbijnokmpl";
+      message = "yp ysii.rs";
+      alphabet = ".waeszrdxtfcygvuhbijnokmpl";
       const actual = substitution(message, alphabet, false);
-      const expected = "my message";
+      expected = "my message";
 
       expect(actual).to.equal(expected);
     });
